b6b088fdc1f0d481ed5f7f0aeb8b1cd5035c4591
PhilosophyWithJosh <joshmendiola@icloud.com>
2023-03-18 18:29:30 -0500

    added post functionality to the blog creation page

--- src/pages/Create.js
+++ src/pages/Create.js
@@ -1,9 +1,56 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 const Create = () => {
+    const [title, setTitle] = useState('')
+    const [author, setAuthor] = useState('')
+    const [body, setBody] = useState('')
+    const [isPending, setIsPending] = useState(false)
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        const blog = {title, body, author};
+
+        setIsPending(true)
+        fetch(`http://localhost:8000/blogs`, {
+            method: 'POST',
+            headers: {"Content-Type": "application/json"},
+            body: JSON.stringify(blog)
+        }).then(() => {
+            console.log("new blog :)");
+            setIsPending(false);
+            setBody('');
+            setAuthor('')
+            setTitle('')
+        })
+    }
+
     return (
-        <div>
-            <h1>Add a new blog :)</h1>
+        <div className="create">
+            <h2>Add New Blog !!</h2>
+            <form onSubmit={handleSubmit}>
+                <label>Blog title:</label>
+                <input
+                    type="text"
+                    required
+                    value={title}
+                    onChange={(e) => setTitle(e.target.value)}
+                />
+                <label>Blog body:</label>
+                <textarea
+                    required
+                    value = {body}
+                    onChange={(e) => setBody(e.target.value)}
+                />
+                <label>Author:</label>
+                <input
+                    type="text"
+                    required
+                    value={author}
+                    onChange={(e) => setAuthor(e.target.value)}
+                />
+                {!isPending && <button> Add Blog </button>}
+                {isPending && <button disabled> Adding blog...</button>}
+            </form>
         </div>
     );
 };
